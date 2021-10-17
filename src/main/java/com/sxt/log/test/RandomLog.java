a12e4a53ad623ffa200380806bffecdb309e38f6
lizeqing <lizeqing77@163.com>
2021-10-17 12:52:41 +0800

    updata at 10/17

---
+++ src/main/java/com/sxt/log/test/RandomLog.java
@@ -0,0 +1,17 @@
+package com.sxt.log.test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class RandomLog {
+	public static String day = "20211017";
+	public static String pre_url = "http%3A%2F%2Flocalhost%3A8080%2FBIG_DATA_LOG2%2Fdemo";
+	static String[]
+}
